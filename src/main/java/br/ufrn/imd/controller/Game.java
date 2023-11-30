5d4c2138f5bcdd5cf4a1403734c35bb5a00bf1e8
arturevs <artur.rp2001@gmail.com>
2023-11-29 22:26:58 -0300

    asta

--- src/main/java/br/ufrn/imd/controller/Game.java
+++ src/main/java/br/ufrn/imd/controller/Game.java
@@ -3,17 +3,17 @@ package br.ufrn.imd.controller;
 import br.ufrn.imd.model.*;
 
 public class Game {
+    private Table tableP1;
+    private Table tableP2;
+    private Player player1;
+    private Player player2;
+
     Game() {
         tableP1 = new Table(10);
         tableP2 = new Table(10);
         player1 = new Player("");
         player2 = new Player("");
     }
-
-    private Table tableP1;
-    private Table tableP2;
-    private Player player1;
-    private Player player2;
 
     // bool addShip(Ship ship) {
     // table.addShip(ship);
