f28eed16015af8f579864fa79f9ce1e29bcabd07
Raquel Freire <raquelcfapoio24@gmail.com>
2023-11-29 22:35:44 -0300

    "game att"

--- src/main/java/br/ufrn/imd/controller/Game.java
+++ src/main/java/br/ufrn/imd/controller/Game.java
@@ -8,18 +8,18 @@ public class Game {
     private Player player1;
     private Player player2;
 
-    Game() {
-        tableP1 = new Table(10);
-        tableP2 = new Table(10);
-        player1 = new Player("");
-        player2 = new Player("");
+    public Game() {
+        this.tableP1 = new Table(10);
+        this.tableP2 = new Table(10);
+        this.player1 = new Player("");
+        this.player2 = new Player("");
     }
 
     // bool addShip(Ship ship) {
     // table.addShip(ship);
     // }
 
-    void startTables() {
+    public void startTables() {
         tableP1.clear();
         tableP2.clear();
     }
