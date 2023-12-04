369d4d538948cf2ad478ac324ac19010d29d7e3b
Mariana Emerenciano <103092034+mariemerenc@users.noreply.github.com>
2023-12-04 11:28:56 -0300

    Função que indica a posição do cursor adicionada

--- src/main/java/br/ufrn/imd/TableController.java
+++ src/main/java/br/ufrn/imd/TableController.java
@@ -15,14 +15,26 @@ public class TableController implements Initializable{
 
     @FXML
     private void initialize() {
+        // Adiciona um ouvinte de evento onMouseClicked ao GridPane
         gridPane.setOnMouseClicked(this::handleGridPaneClick);
+        gridPane.setOnMouseEntered(this::handleGridPaneEnter);
     }
 
     public void setupGridPaneClickEvent() {
+        // Adicione o evento de clique ao GridPane
         gridPane.setOnMouseClicked(this::handleGridPaneClick);
+        gridPane.setOnMouseEntered(this::handleGridPaneEnter);
+    }
+
+    private void handleGridPaneEnter(MouseEvent event){
+        int enteredColumn = (int) (event.getX() / gridPane.getWidth() * gridPane.getColumnCount());
+        int enteredRow = (int) (event.getY() / gridPane.getHeight() * gridPane.getRowCount());
+
+        System.out.println("Cursor na posição: Coluna " + enteredColumn + ", Linha " + enteredRow);
     }
 
     private void handleGridPaneClick(MouseEvent event) {
+        // Lógica a ser executada quando o GridPane for clicado
         int clickedColumn = (int) (event.getX() / gridPane.getWidth() * gridPane.getColumnCount()) + 1;
         int clickedRow = (int) (event.getY() / gridPane.getHeight() * gridPane.getRowCount()) + 1;
 
@@ -32,6 +44,7 @@ public class TableController implements Initializable{
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         gridPane.setOnMouseClicked(this::handleGridPaneClick);
+        gridPane.setOnMouseEntered(this::handleGridPaneEnter);
     }
 
 }
