73947bd3da0124e882032c9be64b0738e9555147
Mariana Emerenciano <103092034+mariemerenc@users.noreply.github.com>
2023-12-10 15:38:09 -0300

    vai fazer sentido juro!!

--- src/main/java/br/ufrn/imd/Player1Positions.java
+++ src/main/java/br/ufrn/imd/Player1Positions.java
@@ -4,14 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Cell;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import br.ufrn.imd.utils.Board;
 
 public class Player1Positions implements Initializable{
 
@@ -47,6 +52,8 @@ public class Player1Positions implements Initializable{
 
 
     DraggableMaker draggableMaker = new DraggableMaker();
+    private Board board;
+
 
 
     @FXML
@@ -107,22 +114,27 @@ public class Player1Positions implements Initializable{
         gridPane.setOnMouseEntered(this::handleGridPaneEnter);
     }
 
-    private void handleGridPaneEnter(MouseEvent event){
+    /*private void handleGridPaneEnter(MouseEvent event) {
         int enteredColumn = (int) (event.getX() / gridPane.getWidth() * gridPane.getColumnCount());
         int enteredRow = (int) (event.getY() / gridPane.getHeight() * gridPane.getRowCount());
 
         System.out.println("Cursor na posição: Coluna " + enteredColumn + ", Linha " + enteredRow);
+
+        Rectangle rectangle = new Rectangle(50, 50 , Color.RED); // Create a rectangle with desired color
+        gridPane.add(rectangle, enteredColumn, enteredRow); // Add the rectangle to the gridPane
     }
 
     private void handleGridPaneClick(MouseEvent event) {
         // Lógica a ser executada quando o GridPane for clicado
         int clickedColumn = (int) (event.getX() / gridPane.getWidth() * gridPane.getColumnCount()) + 1;
         int clickedRow = (int) (event.getY() / gridPane.getHeight() * gridPane.getRowCount()) + 1;
+
+        Cell cell = getCell(clickedColumn, clickedRow);
+
         
         System.out.println("Clicou na posição: Coluna " + clickedColumn + ", Linha " + clickedRow);
 
-    }
-
+    }*/
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -131,8 +143,31 @@ public class Player1Positions implements Initializable{
         draggableMaker.makeDraggable(fragata);
         draggableMaker.makeDraggable(submarino);
 
+        board = new Board();
+        gridPane.getChildren().add(board);
+
         gridPane.setOnMouseClicked(this::handleGridPaneClick);
         gridPane.setOnMouseEntered(this::handleGridPaneEnter);
+    }
+
+        private void handleGridPaneClick(MouseEvent event) {
+        int enteredColumn = (int) (event.getX() / gridPane.getWidth() * gridPane.getColumnCount());
+        int enteredRow = (int) (event.getY() / gridPane.getHeight() * gridPane.getRowCount());
+
+        System.out.println("Cursor na posição: Coluna " + enteredColumn + ", Linha " + enteredRow);
+
+        Rectangle rectangle = new Rectangle(200, 40 , Color.GREENYELLOW); // Create a rectangle with desired color
+        gridPane.add(rectangle, enteredColumn, enteredRow); // Add the rectangle to the gridPane
+    }
+
+    private void handleGridPaneEnter(MouseEvent event) {
+        // Lógica a ser executada quando o GridPane for clicado
+        int clickedColumn = (int) (event.getX() / gridPane.getWidth() * gridPane.getColumnCount()) + 1;
+        int clickedRow = (int) (event.getY() / gridPane.getHeight() * gridPane.getRowCount()) + 1;
+
+        Board.Cell cell = board.getCell(clickedColumn, clickedRow);
+
+        System.out.println("Clicou na posição: Coluna " + clickedColumn + ", Linha " + clickedRow);
     }
 
 }
